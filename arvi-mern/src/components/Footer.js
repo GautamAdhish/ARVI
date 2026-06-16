6f22caab5a15f0813a874d35a93034f05886181b
Adhish Gautam <osiris@kali.kali>
2026-06-16 20:36:31 +0545

    Updated footer section

--- arvi-mern/src/components/Footer.js
+++ arvi-mern/src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
-                <a href="#privacy" className="footer-link">Privacy Policy</a>
-                <a href="#contact" className="footer-link">Contact</a>
+                <Link to="/privacy" className="footer-link">Privacy Policy</Link>
+                <Link to="/contact" className="footer-link">Contact</Link>
             </div>
-            <p>Helping families make mealtimes a little easier. &copy; 2026 ARVI.</p>
+            <p>Helping families make mealtimes a little easier. <br /> &copy; 2026 ARVI.</p>
         </footer>
     );
 };
